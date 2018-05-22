ae4e44d40952aba7a1b907c7c7dd63ab00f2b8a7
sfs-inep <igornepipenko@gmail.com>
2018-05-22 20:51:47 +0300

    lesson 2

--- src/class-work.ts
+++ src/class-work.ts
@@ -36,20 +36,111 @@
 
 // let cb1: new (a: string) => number;
 // let cb2: { new(a: string): number };
-interface IName {
-  readonly firstName: string;
+// interface IName {
+//   readonly firstName: string;
+// }
+//
+// interface IAge {
+//   readonly age: number;
+// }
+//
+// interface IAccount extends IName, IAge {}
+//
+// let person1: IAccount;
+// let person2: IAccount;
+//
+// class A implements IAccount {
+//   public readonly firstName: string = 'asd';
+//   public readonly age: number = 1;
+// }
+// type Account = {
+//   readonly firstName: string,
+//   readonly age: number,
+// };
+// let account: Account;
+//
+// let account1: Account;
+
+// let account2: Account;
+// let s: ReadonlyArray<string | null> = ['asd', 'asd'];
+
+// interface IName<T> {
+//   readonly _id: T;
+//   readonly firstName: string;
+// }
+//
+// let person: IName<number> = {
+//   _id: '1',
+//   firstName: 'Igor',
+// };
+// let person1: IName<string>;
+
+// interface IAccount<MyType extends { firstName: string, age: number }, U> {
+//   id: U;
+//   info: MyType;
+// }
+//
+// let person: IAccount<{ firstName: string, age: number, male: boolean }, string>;
+//
+// function f(c: number, ...a: number[]): number {
+//   return 1;
+// }
+//
+// f(1, 2);
+
+// function isString(c: string | number): c is string {
+//   return typeof c === 'string';
+// }
+//
+// function f(a: number): number;
+// function f(a: string, b: number): string;
+// function f(a: string | number, b?: number): string | number {
+//
+//   let count = 0;
+//   // loop
+//   if (isString(a)) {
+//     count += parseInt(a, 10);
+//   } else {
+//    // a.
+//   }
+//   parseInt(a, 10);
+//
+//
+//   return 1;
+// }
+//
+// f('1', 2);
+
+abstract class SuperPoint {
+  public x!: number;
+
+  public abstract setY(): void;
+
+  public setX() {
+    this.x = 1;
+  }
 }
 
-interface IAge {
-  readonly age: number;
+class Point extends SuperPoint {
+  public x!: number;
+  protected y!: number;
+  private z!: number;
+
+  constructor() {
+    super();
+
+  }
+
+  public setY(): void {
+    this.y = 3;
+  }
 }
 
-interface IAccount extends IName, IAge {}
+let a = new Point();
 
-let person1: IAccount;
-let person2: IAccount;
-
-class A implements IAccount {
-  public readonly firstName: string = 'asd';
-  public readonly age: number = 1;
+class P extends Point {
+  constructor() {
+    super();
+    this.
+  }
 }
